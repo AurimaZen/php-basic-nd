69ccb0c4679df7a1b661b755eac1a969a9339d47
AurimaZen <33114121+AurimaZen@users.noreply.github.com>
2018-03-20 19:15:41 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,42 +5,39 @@ class Ps4Autoloader
 
     public function add(string $prefix, string $path): self
     {
-        $this->rules[] = ['prefix' => $prefix, 'path' => $path];
-
+        // sukurti nauja prefixa ir kaip reiksme nustatyti jo $path
+        $this->rules[$prefix] = $path;
         return $this;
     }
 
     public function register()
     {
-        // TODO: implement this
-
         spl_autoload_register(function ($class) {
 
-            // kelias= return iš add funkcijos
-             $path = $this->rules['path'];
+            foreach($this->rules as $prefix => $path) {
 
-            // tikriname, ar turi prefiksa
-            $len = strlen($prefix);
-            if (strncmp($prefix, $class, $len) !== 0) {
-                // jei ne, baigti
-                return;
+                $len = strlen($prefix);
+                if (strncmp($prefix, $class, $len) !== 0) {
+                    //jei nesutampa klases, nutraukti
+                    return;
+                }
+
+                // gauti relative klases pavadinima
+                $relative_class = substr($class, $len);
+
+
+                $file = $path . str_replace('\\', '/', $relative_class) . '.php';
+
+                if (file_exists($file)) {
+                    require $file;
+                }
             }
-            // gauti relative klases pavadinima
-            $relative_class = substr($class, $len);
 
-            // pakeisti namespace prefiksa i pagrindine direktorija, pakeisti namespace
-            // skirtukus su direktoriju skirtukais in the relativ klases pavadinime,
-            // pridedame .php
-            $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-            // jei failas egzistuoja, reikalaujame
-            if (file_exists($file)) {
-                require $file;
-            }
-        });
+    });
     }
 }
 $autoloader = new Ps4Autoloader();
 $autoloader
     ->add('Nfq\\Academy\\Homework\\', __DIR__.'/src/')
+    //->add('PHP-basic-nd\\Understand\\howitworks\\', __DIR__.'/src/subsrc/thisisit/')
     ->register();
